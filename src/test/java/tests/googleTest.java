13c2670e42a39c136cf00ac22874f5a133280cfa
Nikita Solovev <banalityx@gmail.com>
2021-05-26 22:27:28 +0600

    Добавлен РОМ Переписан первый тест

--- src/test/java/tests/googleTest.java
+++ src/test/java/tests/googleTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pages.SearchPage;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class googleTest {
 
     private static WebDriver driver;
+    private static SearchPage searchPage;
 
     @BeforeAll
     public static void init (){
@@ -22,16 +24,19 @@ public class googleTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
+        searchPage = new SearchPage(driver);
     }
 
     @Test
     public void test1() {
 
                 driver.get("http://google.com");
-                driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("selenium", Keys.ENTER );
-               assertEquals(15, driver.findElements(By.cssSelector("#search .g")).size());
+                searchPage.search("selenium");
+              assertEquals(15, searchPage.results.size());
 
     }
+
+
 
     @AfterAll
     public static void teardown() {
