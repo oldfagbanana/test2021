1c551062c67c65b28790d5bd601d1980c87926f1
Nikita Solovev <banalityx@gmail.com>
2021-05-30 18:00:20 +0600

    Оформлены элементы управления калькулятором

--- src/main/java/pages/SearchPage.java
+++ src/main/java/pages/SearchPage.java
@@ -15,9 +15,64 @@ public class SearchPage {
     @FindBy (css = "input.gLFyf.gsfi")
     private WebElement searchInput;
 
-    @FindBy (css = "#search .g")
-    public List<WebElement> results;
+    @FindBy (xpath = "//*[contains(@jsname, 'j93WEe')]")
+    public WebElement opensk;
 
+
+    @FindBy (xpath = "//*[contains(@jsname, 'qCp9A')]")
+    public WebElement closesk;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'N10B9')]")
+    public WebElement one;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'XSr6wc')]")
+    public WebElement plus;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'lVjWed')]")
+    public WebElement two;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'YovRWb')]")
+    public WebElement mult;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'KN1kY')]")
+    public WebElement three;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'pPHzQc')]")
+    public WebElement minus;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'xAP7E')]")
+    public WebElement four;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'bkEvMb')]")
+    public WebElement zero;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'WxTTNd')]")
+    public WebElement divi;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'Ax5wH')]")
+    public WebElement five;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'abcgof')]")
+    public WebElement six;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'aN1RFf')]")
+    public WebElement sin;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'Pt8tGc')]")
+    public WebElement eq;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'ubtiRe')]")
+    public WebElement string1;
+
+    @FindBy (xpath = "//*[contains(@jsname, 'VssY5c')]")
+    public WebElement string2;
+
+    public String getnumbers() {
+        return string1.getText();
+    }
+    public String getresult() {
+        return string2.getText();
+    }
 
     public SearchPage(WebDriver driver) {
         initElements(driver, this);
